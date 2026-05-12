ffc4502b04b9a933b7124b43c4b95aa616bb7686
literallynoonefr <pedrozcsl@gmail.com>
2026-05-12 19:31:20 -0300

    Adição do Swagger e Atualização do README

--- DemandasComunidade.Api/Program.cs
+++ DemandasComunidade.Api/Program.cs
@@ -6,7 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpClient();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 const string FilePath = "demands.json";
 
